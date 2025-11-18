2b3e71ccb3e0dd6d12bb9ed39c7346213d7b5656
khalidadel <khalidadel209@gmail.com>
2025-11-18 20:22:04 +0200

    finish cubit and states

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:products_task/features/products_list/data/repos/products_repo.dart';
 import 'package:products_task/features/products_list/data/repos/products_repo_imp.dart';
 import 'package:products_task/features/products_list/presentation/manager/products_view_cubit/products_view_cubit.dart';
 import 'package:products_task/features/products_list/presentation/views/products_view.dart';
 
 import 'core/utilities/api_service.dart';
+import 'core/utilities/sevice_locator.dart';
 
 void main() {
   runApp(const ProductsTask());
@@ -17,7 +19,7 @@ class ProductsTask extends StatelessWidget {
   Widget build(BuildContext context) {
     ProductsRepoImp(ApiService()).fetchProducts();
     return BlocProvider(
-      create: (context) => ProductsViewCubit(getIt.get<HomeRepoImp>()),
+      create: (context) => ProductsViewCubit(getIt.get<ProductsRepoImp>()),
       child: MaterialApp(
           debugShowCheckedModeBanner: false,
           home: ProductsView()
